8f52e4063d4b0f0887a307ba025f0390923f5c0b
Valentin Popescu <ppsc.valentin@gmail.com>
2025-04-30 13:47:54 +0300

    Astro config env fields

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+const site = import.meta.env.APP_HOST || "https://ogor.ro"
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ogor.ro",
+  site,
   trailingSlash: "ignore",
   devToolbar: {
     enabled: false
@@ -29,6 +31,15 @@ export default defineConfig({
       }
     }), 
   ],
+  env: {
+    schema: {
+      OGOR_APP_HOST: envField.string({ context: "client", access: "public", default: site }),
+      OGOR_APP_LOGIN_URL: envField.string({ context: "client", access: "public", default: site + "/auth/login" }),
+      OGOR_APP_SIGNUP_URL: envField.string({ context: "client", access: "public", default: site + "/auth/new-account"}),
+      OGOR_BE_HOST: envField.string({ context: "client", access: "public", default: site }),
+      TRACKING_ENABLED: envField.boolean({ context: "client", access: "public", default: false }),
+    }
+  },
   server: {
     allowedHosts: ["ogor.ro"],
     host: true,
